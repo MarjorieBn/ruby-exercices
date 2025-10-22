5e641ba7b3810a1cd44038b27acc9c20193e2c96
Marjorie Bne <marjorie.bne@gmail.com>
2025-10-22 18:52:55 +0200

    rectifications

--- exo_06.rb
+++ exo_06.rb
@@ -1,5 +1,5 @@
 puts "Quel est ton nombre préféré ?"
 num = gets.to_i - 1
-puts "Bonjour toi ! " * num
+puts "Bonjour toi ! \n" * num
 
 # Écris un programme exo_06.rb qui demande un nombre à un utilisateur, puis qui écrit autant de fois -1 "Bonjour toi !". Ainsi, si l'utilisateur rentre 10, le programme devra écrire 9 fois "Bonjour toi !"
--- exo_13.rb
+++ exo_13.rb
@@ -1,4 +1,4 @@
 arr = Array.new(51) {|i| "jean.dupont.#{i.to_s}@email.fr" }
-puts "#{arr}"
+puts arr
 
 # Écris un programme exo_13.rb qui va créer une liste de 50 faux emails et les stocker dans une array.
